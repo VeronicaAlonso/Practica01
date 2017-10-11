b068b69ce1db304ed6bf47b238fe31186d41eb44
VeronicaAlonso <32716284+VeronicaAlonso@users.noreply.github.com>
2017-10-11 13:46:51 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,23 @@
-lugfhdug
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	int vect[21];
+	int aux=0,otro=0;
+	for(int i=0;i<21;i++){
+		printf("Ingresa un numero\n");
+		scanf("%i",&vect[i]);
+}
+	for(int z=0;z<22;z++){
+		for(int y=0;y<22;y++){
+			if(vect[y]>vect[z]){
+				aux=vect[y];
+				vect[y]=vect[z];
+				vect[z]=aux;
+			
+			}
+		}
+	}
+		for(int i=0;i<22;i++){
+		printf("[%i]",vect[i]);}
+}
