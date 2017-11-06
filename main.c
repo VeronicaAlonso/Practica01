fd756892384a036fb4e7e11ad4ae456893f51625
VeronicaAlonso <32716284+VeronicaAlonso@users.noreply.github.com>
2017-11-06 16:29:24 -0600

    Update main.c

--- main.c
+++ main.c
@@ -1,23 +1,53 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+#include<time.h>
 int main(){
-	int vect[21];
-	int aux=0,otro=0;
-	for(int i=0;i<21;i++){
-		printf("Ingresa un numero\n");
-		scanf("%i",&vect[i]);
-}
-	for(int z=0;z<22;z++){
-		for(int y=0;y<22;y++){
-			if(vect[y]>vect[z]){
-				aux=vect[y];
-				vect[y]=vect[z];
-				vect[z]=aux;
-			
+	int vect[5];
+	int buscado,mitad;
+	bool band=false;
+	int i,j,aux;
+	printf("Numeros aleatorios del 1 al 10\n\n");
+	srand(time(NULL));
+	for (int i = 0; i < 5; i ++){
+		vect[i] =1+rand()%((10+1)-1);
+		printf("[%i]",vect[i]);
+	}
+	
+	for(i=0;i<5;i++){
+		for(j=0;j<5;j++){
+			if(vect[j]>vect[j+1]){
+				aux=vect[j];
+				vect[j]=vect[j+1];
+				vect[j+1]=aux;
 			}
 		}
 	}
-		for(int i=0;i<22;i++){
-		printf("[%i]",vect[i]);}
+	printf("El orden es:");
+	for(i=0;i<5;i++){
+		printf("[%i]",vect[i+1]);
+	}printf("\n\n");
+
+	printf("Ingresa el numero que deseas buscar\n\n");
+	scanf("%i",&buscado);
+	int inf=0, sup=5;
+	while (inf<=sup){
+		mitad=(inf+sup)/2;
+		if(vect[mitad]==buscado){
+			band=true;
+			break;
+		}
+		if(vect[mitad]<buscado){
+			inf=mitad;
+			mitad=(inf+sup)/2;
+		}		
+		if(vect[mitad]>buscado){
+			sup=mitad;
+			mitad=(inf+sup)/2;
+		}}
+		if(band==false){
+		printf("El numero no existe\n");	
+		}
+	else if(band==true){
+		printf("Encontrado en la posicion [%i]",mitad);
+	}
 }
